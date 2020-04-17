7362c7f61f34024fdc38456052d34b2d5f125c67
Glitch (helix-yielding-hair) <none>
2020-04-17 00:17:35 +0000

    🌟🏆 Updated with Glitch

---
+++ src/TodoData.js
@@ -0,0 +1,34 @@
+const TodoData = [
+  {
+    id: 1,
+    task: "Buy groceries",
+    importance: "low",
+    completed: true
+  },
+  {
+    id: 2,
+    task: "Take out trash",
+    importance: "low",
+    completed: true
+  },
+  {
+    id: 3,
+    task: "Buy dog food",
+    importance: "high",
+    completed: false
+  },
+  {
+    id: 4,
+    task: "Walk dogs",
+    importance: "low",
+    completed: false
+  },
+  {
+    id: 5,
+    task: "Apply for jobs",
+    importance: "high",
+    completed: true
+  }  
+];
+
+export default TodoData;
